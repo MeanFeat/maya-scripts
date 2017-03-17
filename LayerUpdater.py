37648739575c9cdf9d71f0979023e3e09cf6a367
MeanFeat <jlecoui@msn.com>
2017-03-16 17:21:35 -0700

    fix

--- LayerUpdater.py
+++ LayerUpdater.py
@@ -6,6 +6,8 @@ def CreateScriptJob():
     return cmds.scriptJob(e=("animLayerRefresh", UpdateSelectedAnimLayer), killWithScene=True)
 
 def GetFrameRange( sel ):
+    if sel is None:
+        return ''
     splitName = sel.split('_')
     endFrame = splitName[-1]
     endFrame = endFrame.replace("f", "")
@@ -14,20 +16,43 @@ def GetFrameRange( sel ):
 def UpdateSelectedAnimLayer():
     for item in cmds.ls(type='animLayer'):
         if (cmds.animLayer(item,q=True,selected=True)):
-			UpdateAnimLayer( item )
+            UpdateAnimLayer( item )
+
+def GetParentLayer( layer ):
+    return cmds.animLayer(layer, q=True, parent = True )
+
+def GetParents ( item, layers):
+    done = False;
+    while not done:
+        done = True
+        item = GetParentLayer(item)
+        if item != 'BaseAnimation':
+            layers.append(item)
+            done = False
+    return layers
+
+def GetChildren(item, layers):
+    children = cmds.animLayer(item, q=True, children=True)            
+    if children != None:
+        for c in children:
+            layers.append(c)
+    return layers
 
 def UpdateAnimLayer( layer ):
+    unmute = []
     for item in cmds.ls(type='animLayer'):
         if (item == layer):
-            par = cmds.animLayer(item, q=True, parent=True) 
-            cmds.setAttr(par + ".mute", 0)   
-            cmds.setAttr(item + ".mute", 0)
-            cmds.setAttr(item + ".lock", 0)
             if(GetFrameRange(item).isdigit()):
                 cmds.playbackOptions(max=GetFrameRange(item))
+            unmute.append(item)
+            unmute = GetParents(item, unmute)
+            unmute = GetChildren(item, unmute)
         elif item != 'BaseAnimation':
             cmds.setAttr(item + ".lock", 1)
-            cmds.setAttr(item + ".mute", 1)		
+            cmds.setAttr(item + ".mute", 1)
+    for u in unmute:
+        cmds.setAttr(u + ".mute", 0)
+        cmds.setAttr(u + ".lock", 0)
 
 def SelectExportJoints():
     cmds.select('Rig:root', hi=True)
@@ -44,43 +69,57 @@ def GetExportName(exportLayer):
     return exportLayerName[:len(exportLayerName)-1]
 
 def GetExportSettings( exportLayer, start, end, fileName = ''):
-    string = 'file -force -options "v=0;" -typ "FBX export" -pr -es "' + snip.GetFilePath() + "FBX/" 
-    mel.eval('FBXExportBakeComplexAnimation -v true;')
+    mel.eval('FBXExportBakeComplexAnimation -v 1;')
+    mel.eval('FBXExportConstraints -v 0;')
+    mel.eval('FBXExportInputConnections -v 0;')
+    mel.eval('FBXExportShapes -v 1;')
+    mel.eval('FBXExportSmoothMesh -v 1;')
     min = str(0) if start == None else str(start)
     layerFrameRange = GetFrameRange(exportLayer)
     if layerFrameRange.isdigit():
         max = layerFrameRange
     max = max if end == None else str(end)
+    mel.eval('FBXExportBakeComplexAnimation -v true;')
     mel.eval("FBXExportBakeComplexStart -v " + min) 
     mel.eval("FBXExportBakeComplexEnd -v " + max)
-    if len(fileName) == 0:
+    if fileName == None or len(fileName) == 0:
         fileName = GetExportName(exportLayer);
-    return string + fileName + '.fbx";'
-    
-def ExportLayer( settings ):
-    print settings
-    origSelection = cmds.ls(selection=True)
-    SelectExportJoints()   
-    mel.eval(settings)
-    cmds.select(origSelection)	
+    return 'FBXExport -f "' + snip.GetFilePath() + "/FBX/" + fileName + '.fbx";'
 
-def ExportSelectedLayers():
+def IsExportable(item):
+    return GetFrameRange(item).isdigit() 
+
+def ExportLayer( settings ):
+    origSelection = cmds.ls(selection=True)
+    SelectExportJoints() 
+    print settings  
+    mel.eval(settings)
+    cmds.select(origSelection)  
+
+def GetSelectedLayers():
+    layers = []
     for item in cmds.ls(type='animLayer'):
         if (cmds.animLayer(item,q=True,selected=True)):
-            UpdateAnimLayer( item )
-            ExportLayer ( GetExportSettings(item, None, None) )
+            layers.append(item)
+    return layers
+
+def ExportSelectedLayers():
+    for item in GetSelectedLayers():
+            if IsExportable(item):
+                UpdateAnimLayer( item )
+                ExportLayer ( GetExportSettings(item, None, None) )
 
 def ExportLayerRange(start, end, name):
     cmds.playbackOptions(min=start,max=end) 
     ExportLayer ( GetExportSettings(None, start, end, name))  
     UpdateSelectedAnimLayer()
-	
+    
 def ExportAll():
-	for item in cmds.ls(type='animLayer'):
-		if item != 'BaseAnimation':
-			if GetFrameRange(item).isdigit():
-			    UpdateAnimLayer(item)
-			    ExportLayer(item)
+    for item in cmds.ls(type='animLayer'):
+        if item != 'BaseAnimation':
+            if IsExportable(item):
+                UpdateAnimLayer(item)
+                ExportLayer(item)
                 
 def TryExportRange():
     start = cmds.intFieldGrp('rangeFields', query=True, value1=True)
@@ -94,11 +133,10 @@ def TryExportRange():
 
 
 def ExportRangeWindow():
-    menuWin = cmds.window( title="Trajectory Guide", iconName='TrajectoryGuide', resizeToFitChildren=True, te=50,le=300, widthHeight=(50, 50))
+    menuWin = cmds.window( title="Export Layer Range", iconName='Export Layer Range', resizeToFitChildren=True, te=50,le=300, widthHeight=(50, 50))
     cmds.columnLayout( adjustableColumn=True )
     cmds.intFieldGrp('rangeFields', numberOfFields=2, label='Range', value1=0, value2=20, columnAlign2=('Left','Left'))
     cmds.textField('nameField', width = 50)
     cmds.button(label='Export', command=('TryExportRange()'))
     cmds.setParent( '..' )
     cmds.showWindow( menuWin )
-
