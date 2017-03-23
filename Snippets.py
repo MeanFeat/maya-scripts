5ac8b0a91763ed314a4598b820114abd745e7edf
MeanFeat <jlecoui@msn.com>
2017-03-23 14:45:51 -0700

    Filename functions

--- Snippets.py
+++ Snippets.py
@@ -13,3 +13,13 @@ def FailExit(message):
     cmds.error(message)
     sys.exit()
 
+def GetFileName():
+	fileName = cmds.file(query = True, expandName=True)
+	fileName = fileName.split('.')
+	fileName = fileName[0].split('/')
+	return fileName[-1]
+
+def GetFileExtension():
+	fileName = cmds.file(query = True, expandName=True)
+	fileName = fileName.split('.')
+	return '.' + fileName[-1]
