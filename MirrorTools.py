92c3f0921288e4330a074194850d10ee10042cfc
MeanFeat <jlecoui@msn.com>
2017-04-01 00:50:31 -0700

    Mirror tweaks

--- MirrorTools.py
+++ MirrorTools.py
@@ -7,37 +7,41 @@ import maya.cmds as cmds
 import maya.mel as mel
 import maya
 import sys
+import maya.api.OpenMaya as om
+import LayerTools as layerTools
 
 class option():
-    keys = 0
-    curve = 1
-    def __init__(self, Type):
-        self.value = Type
-    def __str__(self):
-        if self.value == option.keys:
-            return 'keys'
-        if self.value == option.curve:
-            return 'curve'
-    def __eq__(self,y):
-       return self.value==y.value
-
+	keys = 0
+	curve = 1
+	def __init__(self, Type):
+		self.value = Type
+	def __str__(self):
+		if self.value == option.keys:
+			return 'keys'
+		if self.value == option.curve:
+			return 'curve'
+	def __eq__(self,y):
+	   return self.value==y.value
 
 def SelectControls():
-    for item in cmds.ls(type='animLayer'):
-        if (cmds.animLayer(item,q=True,selected=True)):
-            mel.eval('string $layers[]={"'+item+'"};')
-            mel.eval('layerEditorSelectObjectAnimLayer($layers);')
+	for item in cmds.ls(type='animLayer'):
+		if (cmds.animLayer(item,q=True,selected=True)):
+			mel.eval('string $layers[]={"'+item+'"};')
+			mel.eval('layerEditorSelectObjectAnimLayer($layers);')
 
 def SwapAnimation(a, b, optionType):
 	null = cmds.spaceLocator(name="tempCopyNull")
 	cmds.select(null,r=True)
-	cmds.animLayer(addSelectedObjects=True)
-	if cmds.selectKey( a ) > 0:
-		SendAnimation( a, null[0], optionType) 
+	cmds.animLayer(layerTools.GetSelectedLayers()[0], edit=True, addSelectedObjects=True)
+	if cmds.selectKey( a ) > 0:		
+		print "A has animation"
+		SendAnimation( a, null, optionType)
 	if cmds.selectKey( b ) > 0:
+		print "B has animation"
 		SendAnimation( b, a, optionType )
-	if cmds.selectKey( null[0] ) > 0:
-		SendAnimation( null[0], b, optionType)
+	if cmds.selectKey( null) > 0:
+		print "Null has animation"
+		SendAnimation( null, b, optionType)
 	cmds.delete(null)
 
 def SendAnimation(a,b, optionType):
@@ -49,7 +53,7 @@ def SendAnimation(a,b, optionType):
 		cmds.pasteKey( b )
   
 def MirrorAnimation( item, optionType ):
-	attr = ("translateY", "rotate", "rotateZ")    
+	attr = ("translateY", "rotateX", "rotateZ")    
 	for a in attr:
 		cmds.select(cl=True)
 		key = cmds.selectKey( item, attribute=a)
@@ -62,45 +66,44 @@ def MirrorAnimation( item, optionType ):
 	cmds.select( item )
 
 def PopulateLists():
-    SelectControls()
-    ctrls = cmds.ls(selection=True)
-    tempLeft=[]
-    tempRight=[]
-    for c in ctrls:
-        ctrlName = c.split(':') #ignore Namespace
-        ctrlName = ctrlName[len(ctrlName)-1]
-        if ( "l_" in ctrlName ):
-            left.append(c)
-        elif ( "r_" in ctrlName ):
-            right.append(c)
-        elif ( "c_" in ctrlName ):
-            single.append(c)
-    tempRight.sort()
-    tempLeft.sort()
-    if len(tempRight) != len(tempRight):
-        for le in tempLeft:
-            L_token = le.split('_')[-1]
-            for ri in tempRight: 
-                R_token = ri.split('_')[-1]
-                if (R_token == L_token) and ( "l_" in le ) and ("r_" in ri):
-                    if le not in left:
-                        left.append(le)
-                    if ri not in right:
-                        right.append(ri)
-        cmds.error("Sides are not symetrical")
+	SelectControls()
+	ctrls = cmds.ls(selection=True)
+	tempLeft=[]
+	tempRight=[]
+	for c in ctrls:
+		ctrlName = c.split(':') #ignore Namespace
+		ctrlName = ctrlName[len(ctrlName)-1]
+		if ( "l_" in ctrlName ):
+			tempLeft.append(c)
+		elif ( "r_" in ctrlName ):
+			tempRight.append(c)
+		elif ( "c_" in ctrlName ):
+			single.append(c)
+	tempRight.sort()
+	tempLeft.sort()
+	if len(tempLeft) != len(tempRight):
+		for le in tempLeft:
+			L_token = le.split('_')[-1]
+			for ri in tempRight: 
+				R_token = ri.split('_')[-1]
+				if (R_token == L_token) and ( "l_" in le ) and ("r_" in ri):
+					if le not in left:
+						left.append(le)
+					if ri not in right:
+						right.append(ri)
 
 def MirrorAll(optionType = option.keys):
-    global cTime
-    cTime = int(cmds.currentTime(query=True))
-    global single
-    single = []
-    global right
-    right = []
-    global left
-    left = []
-    PopulateLists()
-    for ind in range(0,len(left)):
-        SwapAnimation(left[ind],right[ind], optionType)        
-    for s in single:
-        MirrorAnimation(s, optionType) 
+	global cTime
+	cTime = int(cmds.currentTime(query=True))
+	global single
+	single = []
+	global right
+	right = []
+	global left
+	left = []
+	PopulateLists()
+	for ind in range(0,len(left)):
+		SwapAnimation(left[ind],right[ind], optionType)        
+	for s in single:
+		MirrorAnimation(s, optionType) 
 
