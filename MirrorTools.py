7634975072efe2ada48589c19b78136840521cd6
MeanFeat <jlecoui@msn.com>
2017-04-02 01:58:05 -0700

    Mirror tools build basis

--- MirrorTools.py
+++ MirrorTools.py
@@ -9,6 +9,25 @@ import maya
 import sys
 import maya.api.OpenMaya as om
 import LayerTools as layerTools
+import collections
+
+BasisTuple = collections.namedtuple('object', ['up','forward','right'])
+
+class axis():
+	X = 0
+	Y = 1
+	Z = 2
+	def __init__(self, Type):
+		self.value = Type
+	def __str__(self):
+		if self.value == axis.X:
+			return 'X'
+		if self.value == axis.Y:
+			return 'Y'
+		if self.value == axis.Z:
+			return 'Z'
+	def __eq__(self,y):
+	   return self.value==y.value
 
 class option():
 	keys = 0
@@ -51,9 +70,12 @@ def SendAnimation(a,b, optionType):
 		else:
 			cmds.copyKey(a, option=option(optionType) );
 		cmds.pasteKey( b )
-  
+ 
+def GetMirroredScaleMatrix():
+	return
+
 def MirrorAnimation( item, optionType ):
-	attr = ("translateY", "rotateX", "rotateZ")    
+	attr = ("translateX","translateY","translateZ", "rotateX", "rotateY", "rotateZ")   
 	for a in attr:
 		cmds.select(cl=True)
 		key = cmds.selectKey( item, attribute=a)
@@ -103,7 +125,36 @@ def MirrorAll(optionType = option.keys):
 	left = []
 	PopulateLists()
 	for ind in range(0,len(left)):
-		SwapAnimation(left[ind],right[ind], optionType)        
+		SwapAnimation(left[ind],right[ind], optionType)
 	for s in single:
 		MirrorAnimation(s, optionType) 
 
+def CreateNullOnObject(item, suffix):
+	tempNull = cmds.group( empty=True, name = item + suffix )
+	tempPC = cmds.parentConstraint( item, tempNull, mo=False)
+	cmds.delete(tempPC)
+	cmds.makeIdentity(tempNull,apply=True, t=1, r=1, s=1)
+	return tempNull
+
+def BuildDirVector(item, transAxis):
+	center = cmds.xform(sel[0], query=True, translation=True, worldSpace=True)
+	null = CreateNullOnObject(item, '_tempNull_' + transAxis.__str__())
+	cmds.move( 1,null,localSpace=True, x=(transAxis == axis(axis.X)), y=(transAxis == axis(axis.Y)), z=(transAxis == axis(axis.Z)))
+	end = cmds.xform(null, query=True, translation=True, worldSpace=True)
+	cmds.delete(null)
+	direction = om.MVector(end[0] - center[0], end[1] - center[1], end[2] - center[2])
+	direction.normalize()
+	return direction
+
+def BuildBasis(item):
+	axes = []
+	axes.append(BuildDirVector(item, axis(axis.X)))
+	axes.append(BuildDirVector(item, axis(axis.Y)))
+	axes.append(BuildDirVector(item, axis(axis.Z)))
+	for a in axes:
+		print a.__str__()
+	#return BasisTuple( item, axes)
+
+cmds.select('Rig:l_legCtrl')
+sel = cmds.ls(selection=True)
+tempBasis = BuildBasis(sel[0])
